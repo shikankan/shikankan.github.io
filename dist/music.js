2dba43bb3337c9a33eda6b4a5917d3c6132367cd
shikankan <xiaopengzhang1314@gmail.com>
2018-07-11 04:11:19 +0800

    Site updated: 2018-07-11 04:11:16

---
+++ dist/music.js
@@ -0,0 +1,13 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    audio: [
+      {
+        name: "いつも何度でも",
+        artist: '宗次郎',
+        url: 'http://www.ytmp3.cn/down/43584.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/74.jpg',
+      }
+    ]
+});
